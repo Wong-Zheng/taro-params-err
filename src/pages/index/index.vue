7790e73300e504f93a15f68e1b747f989f983230
WangZheng <809009636@qq.com>
2022-05-15 16:45:26 +0800

    taro 路由参数无法获取

---
+++ src/pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="index">
+    <button @click="gotoModel">goto model</button>
+  </view>
+</template>
+
+<script>
+import Taro from "@tarojs/taro";
+
+export default {
+  name: "Index",
+  components: {},
+  setup() {
+    const gotoModel = () => {
+      Taro.navigateTo({
+        url: "/pages/model/model?mid=8",
+      });
+    };
+
+    return {
+      gotoModel,
+    };
+  },
+};
+</script>
+
+<style>
+.index {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/pages/model/model.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="model">
+    <h1>mid: {{ mid }}</h1>
+  </view>
+</template>
+
+<script>
+import { onMounted, ref } from "vue";
+import Taro from "@tarojs/taro";
+
+export default {
+  name: "Model",
+  components: {},
+  setup() {
+    let mid = ref("");
+
+    onMounted(() => {
+      mid.value = Taro.getCurrentInstance().router.params.mid;
+      console.log("onMounted---params", mid.value);
+    });
+
+    return {
+      mid,
+    };
+  },
+};
+</script>
+
+<style>
+.model {
+}
+</style>
